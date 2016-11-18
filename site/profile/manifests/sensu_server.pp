d0922ddc42b609849c0d2b05beba8986827ef8eb
Jesse Reynolds <jesse@va.com.au>
2016-11-18 15:44:45 +1030

    use variables or facts to pick up sensu account details

--- site/profile/manifests/sensu_server.pp
+++ site/profile/manifests/sensu_server.pp
@@ -1,17 +1,20 @@
 class profile::sensu_server {
+
+  unless $::sensu_enterprise_user and $::sensu_enterprise_pass {
+    fail('Please set $::sensu_enterprise_user and $::sensu_enterprise_password as variables or external facts. These should correspond to your account with Sensu.')
+  }
+
   class { '::sensu':
-    install_repo      => true,
-    #server            => true,
-    enterprise        => true,
+    install_repo         => true,
+    enterprise           => true,
     enterprise_dashboard => true,
-    manage_services   => true,
-    manage_user       => true,
-    rabbitmq_password => 'fandango-tulip-soldier',
-    rabbitmq_vhost    => '/sensu',
-    #api               => true,
-    #api_user          => 'admin',
-    #api_password      => 'secret',
-    client_address    => $::ipaddress,
+    enterprise_user      => $::sensu_enterprise_user,
+    enterprise_pass      => $::sensu_enterprise_password,
+    manage_services      => true,
+    manage_user          => true,
+    rabbitmq_password    => 'fandango-tulip-soldier',
+    rabbitmq_vhost       => '/sensu',
+    client_address       => $::ipaddress,
   }
 
   sensu::check { 'check_ntp':
