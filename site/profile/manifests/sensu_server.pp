b260d08c2456494138753de8f46955792a23d463
Jesse Reynolds <jesse@va.com.au>
2016-11-18 17:22:34 +1030

    add some debugging

--- site/profile/manifests/sensu_server.pp
+++ site/profile/manifests/sensu_server.pp
@@ -4,6 +4,11 @@ class profile::sensu_server {
     fail('Please set $::sensu_enterprise_user and $::sensu_enterprise_password as variables or external facts. These should correspond to your account with Sensu.')
   }
 
+  $u_type = type($::sensu_enterprise_user)
+  $p_type = type($::sensu_enterprise_password)
+
+  notify {"types: ${u_type}, ${p_type}": }
+
   class { '::sensu':
     install_repo         => true,
     enterprise           => true,
